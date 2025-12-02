1ceb25b73b019a5855aef60dad0ece31ce2892e1
Mikey Stengel <mikey.stengel96@gmail.com>
2025-12-02 14:59:35 +0100

    fix: adjust default svg size

--- src/components/FontAwesomeIcon.tsx
+++ src/components/FontAwesomeIcon.tsx
@@ -1,95 +1,61 @@
-import React, { type SVGAttributes, type Ref } from 'react'
+import React, { type SVGAttributes, CSSProperties } from 'react'
 
 import type { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 
 export interface FontAwesomeIconProps extends SVGAttributes<SVGSVGElement> {
   icon: IconDefinition
-  ref?: Ref<SVGSVGElement>
 }
 
+/**
+ * Minimal, fast Font Awesome React wrapper.
+ * - Uses 1em sizing so font-size (e.g. Tailwind text-xs) controls icon size
+ * - Uses currentColor for fill so text color controls icon color
+ * - No forwardRef, no special ref prop
+ */
 export const FontAwesomeIcon = ({
   icon,
   className,
-  ref,
-  ...rest
+  style,
+  ...restProps
 }: FontAwesomeIconProps) => {
   const iconData = icon.icon
-  const width = iconData[0]
-  const height = iconData[1]
-  const path = iconData[4]
+  const viewBoxWidth = iconData[0]
+  const viewBoxHeight = iconData[1]
+  const svgPathData = iconData[4]
+
+  const mergedStyle: CSSProperties = {
+    display: 'inline-block',
+    verticalAlign: '-0.125em',
+    overflow: 'visible',
+    // 1em means "whatever the specified font-size is"
+    width: '1em',
+    height: '1em',
+    // user styles can override these if they want
+    ...style,
+  }
+
+  const pathDefinitions =
+    typeof svgPathData === 'string' ? [svgPathData] : svgPathData
+
+  const ariaLabel = (restProps as SVGAttributes<SVGSVGElement>)['aria-label']
+  const hasAriaLabel = Boolean(ariaLabel)
 
   return (
     <svg
-      ref={ref}
       xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${width} ${height}`}
-      aria-hidden="true"
+      viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
+      width="1em"
+      height="1em"
+      aria-hidden={hasAriaLabel ? undefined : 'true'}
+      role={hasAriaLabel ? 'img' : undefined}
       focusable="false"
       className={className}
+      style={mergedStyle}
       fill="currentColor"
-      {...rest}>
-      {typeof path === 'string' ? (
-        <path d={path} />
-      ) : (
-        path.map((d, i) => <path key={i} d={d} />)
-      )}
+      {...restProps}>
+      {pathDefinitions.map((pathDefinition, index) => (
+        <path key={index} d={pathDefinition} />
+      ))}
     </svg>
   )
 }
-
-FontAwesomeIcon.displayName = 'FontAwesomeIcon'
-
-// Version 2: forwardRef only (no memo)
-// export const FontAwesomeIconNoMemo = forwardRef<SVGSVGElement, FontAwesomeIconProps>(
-//   ({ icon, className, ...rest }, ref) => {
-//     const iconData = icon.icon
-//     const width = iconData[0]
-//     const height = iconData[1]
-//     const path = iconData[4]
-
-//     return (
-//       <svg
-//         ref={ref}
-//         xmlns="http://www.w3.org/2000/svg"
-//         viewBox={`0 0 ${width} ${height}`}
-//         aria-hidden="true"
-//         focusable="false"
-//         className={className}
-//         fill="currentColor"
-//         {...rest}
-//       >
-//         {typeof path === 'string' ? (
-//           <path d={path} />
-//         ) : (
-//           path.map((d, i) => <path key={i} d={d} />)
-//         )}
-//       </svg>
-//     )
-//   }
-// )
-// FontAwesomeIconNoMemo.displayName = 'FontAwesomeIconNoMemo'
-
-// // Version 3: Bare minimum - no memo, no forwardRef, no rest spread
-// export const FontAwesomeIconBare = ({ icon, className }: { icon: IconDefinition; className?: string }) => {
-//   const iconData = icon.icon
-//   const width = iconData[0]
-//   const height = iconData[1]
-//   const path = iconData[4]
-
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       viewBox={`0 0 ${width} ${height}`}
-//       aria-hidden="true"
-//       focusable="false"
-//       className={className}
-//       fill="currentColor"
-//     >
-//       {typeof path === 'string' ? (
-//         <path d={path} />
-//       ) : (
-//         path.map((d, i) => <path key={i} d={d} />)
-//       )}
-//     </svg>
-//   )
-// }
